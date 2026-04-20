ef3839175d6727aadc67fd1dcd9de63f4158ce85
Juan Manuel Gelay <juanmanuelgelay@gmail.com>
2026-04-19 21:36:25 -0300

    Created a variable for speed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 15.0f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +15,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // We'll move the vehicle forward.
-        transform.Translate(Vector3.forward);
+        // We'll move the vehicle forward 20 m/s.
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
